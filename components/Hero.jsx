853856921a16bb908c4713eca5d6e53a4caa3909
Abdullah <A21alharbi9@gmail.com>
2022-01-10 22:21:25 +0300

    Adjustments to font and colors

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,10 +10,13 @@ function Hero() {
             We empower
           </h1>
           <span className="text-white text-lg lg:text-2xl font-mono ml-2 lg:ml-3">
-            Your dreams
+            Your
+            <span className="text-cyan-300 text-3xl lg:text-5xl font-bold ml-2 lg:ml-3 drop-shadow shadow-neutral-100 ">
+              Dreams
+            </span>
           </span>
         </div>
-        <div className="py-2 ml-2">
+        <div className="flex py-2 ml-2">
           <button className="bg-orange-600 text-white text-lg rounded-lg py-2 px-3 text-center cursor-pointer hover:bg-orange-600 hover:text-black transition duration-500">
             Learn more
           </button>
@@ -27,7 +30,7 @@ function Hero() {
           placeholder="main_image"
           layout="responsive"
           width={750}
-          height={350}
+          height={340}
         />
       </div>
     </div>
