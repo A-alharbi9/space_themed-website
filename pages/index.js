d55608337c06d622873a07170d265f7fe564a36d
Abdullah <A21alharbi9@gmail.com>
2022-12-11 16:11:59 +0300

    Added OG meta tags

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,31 @@ export default function Home() {
         <div>
             <Head>
                 <title>Home | Atlacore</title>
+                <meta property="og:url" content="https://space-themed-website.vercel.app/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Atlacore - Space Website" />
+                <meta
+                    property="og:description"
+                    content="A space themed website made with Next.js framework."
+                />
+                <meta
+                    name="og:image"
+                    content="https://space-themed-website.vercel.app/images/main_image.webp"
+                />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="space-themed-website.vercel.app" />
+                <meta property="twitter:url" content="https://space-themed-website.vercel.app/" />
+                <meta name="twitter:title" content="Atlacore - Space Website" />
+                <meta
+                    name="twitter:description"
+                    content="A space themed website made with Next.js framework."
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://space-themed-website.vercel.app/images/main_image.webp"
+                />
+
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="description" content="Space themed website" />
                 <link rel="icon" href="/favicon.ico" />
